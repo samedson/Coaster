317ab3821ab1d6d6d57cb6c50e0a500874da7676
fox11trot <fox11trot@hotmail.com>
2014-07-21 17:14:20 -0700

    Initial Commit

---
+++ Coaster/CoasterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CoasterAppDelegate.h
+//  Coaster
+//
+//  Created by Samuel Edson on 7/21/14.
+//  Copyright (c) 2014 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoasterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Coaster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Coaster
+//
+//  Created by Samuel Edson on 7/21/14.
+//  Copyright (c) 2014 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CoasterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CoasterAppDelegate class]));
+  }
+}
