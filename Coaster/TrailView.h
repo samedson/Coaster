912508e5a4cdbe901707b0ac14513707868b43ce
fox11trot <fox11trot@hotmail.com>
2014-07-22 20:26:31 -0700

    Removed Unnecessary Comments

--- Coaster/TrailView.h
+++ Coaster/TrailView.h
@@ -11,22 +11,3 @@
 @interface TrailView : UIView
 
 @end
-
-//      if ((yCircle - yScroll) >= yMax) { // Bottom
-//        circle.frame = CGRectMake(-1 * yScroll + (yCircle - self.frame.size.height),
-//                                  yMax + yScroll,
-//                                  2 * kCellRadius,
-//                                  2 * kCellRadius);
-//      }
-//      if (circle.frame.origin.x <= 0) { // Left
-//        circle.frame = CGRectMake(0,
-//                                  yCircle + kLeftOffset,
-//                                  2 * kCellRadius,
-//                                  2 * kCellRadius);
-//      }
-//      if (circle.frame.origin.x >= xRight) { // Right
-//        circle.frame = CGRectMake(xRight,
-//                                  2 * yScroll + (yMax - yCircle) + rightOffset,
-//                                  2 * kCellRadius,
-//                                  2 * kCellRadius);
-//      }
